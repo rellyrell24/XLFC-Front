47e6949b738e3dda5f091472e87694e7c5735dab
Sammy087 <144963396+Sammy087@users.noreply.github.com>
2024-09-27 15:51:21 -0500

    delete screens update

---
+++ XLFC/reactscreens/Admin/delete/choosedelete.jsx
@@ -0,0 +1,761 @@
+
+import 
+ 
+    React 
+
+ 
+
+from
+"react"
+import 
+ 
+    62ed26e70a7ec30939dc5e5fBgBlurLight12 
+
+ 
+
+from
+"./62ed26e70a7ec30939dc5e5f-BG-blur-light-1-2.png"
+import 
+ 
+    bookmark3 
+
+ 
+
+from
+"./bookmark-3.svg"
+import 
+ 
+    cap 
+
+ 
+
+from
+"./cap.svg"
+import 
+ 
+    cellularConnection 
+
+ 
+
+from
+"./cellular-connection.svg"
+import 
+ 
+    chat2 
+
+ 
+
+from
+"./chat-2.svg"
+import 
+ 
+    discount1 
+
+ 
+
+from
+"./discount-1.svg"
+import 
+ 
+    frame60 
+
+ 
+
+from
+"./frame-60.svg"
+import 
+ 
+    home 
+
+ 
+
+from
+"./home.png"
+import 
+ 
+    layer71 
+
+ 
+
+from
+"./layer-7-1.png"
+import 
+ 
+    loupe1 
+
+ 
+
+from
+"./loupe-1.svg"
+import 
+ 
+    setting 
+
+ 
+
+from
+"./setting.svg"
+import "./style.css"
+import 
+ 
+    wifi 
+
+ 
+
+from
+"./wifi.svg"
+
+export const ChooseTeamTo = () => {
+    
+
+    return (
+        <div
+    
+        className= "choose-team-to" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "overlap-wrapper" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "overlap" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<img
+    
+        className= "img" 
+
+    
+
+    
+
+    alt= "Img" 
+src={ 62ed26e70a7ec30939dc5e5fBgBlurLight12 }
+
+
+    
+
+
+/>
+<div
+    
+        className= "frame" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "sarah-collin" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+Search for a team
+</div>
+<img
+    
+        className= "loupe" 
+
+    
+
+    
+
+    alt= "Loupe" 
+src={ loupe1 }
+
+
+    
+
+
+/>
+</div>
+<div
+    
+        className= "fill-your-profile" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+Which team?
+</div>
+<div
+    
+        className= "status-bar" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "time" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "text-wrapper" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+9:41
+</div>
+</div>
+<div
+    
+        className= "levels" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "battery" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "overlap-group" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "capacity" 
+
+    
+
+    
+
+    
+
+    
+
+
+/>
+</div>
+<img
+    
+        className= "cap" 
+
+    
+
+    
+
+    alt= "Cap" 
+src={ cap }
+
+
+    
+
+
+/>
+</div>
+<img
+    
+        className= "wifi" 
+
+    
+
+    
+
+    alt= "Wifi" 
+src={ wifi }
+
+
+    
+
+
+/>
+<img
+    
+        className= "cellular-connection" 
+
+    
+
+    
+
+    alt= "Cellular connection" 
+src={ cellularConnection }
+
+
+    
+
+
+/>
+</div>
+</div>
+<div
+    
+        className= "div" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "email" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+Teams
+</div>
+<div
+    
+        className= "frame-2" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "frame-3" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "type-edit-avatar" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<img
+    
+        className= "layer" 
+
+    
+
+    
+
+    alt= "Layer" 
+src={ layer71 }
+
+
+    
+
+
+/>
+</div>
+<div
+    
+        className= "text-wrapper-2" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+Readings XLFC
+</div>
+</div>
+<div
+    
+        className= "frame-4" 
+
+    
+
+    
+
+    
+
+    
+
+
+/>
+</div>
+</div>
+<div
+    
+        className= "frame-5" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<img
+    
+        className= "home" 
+
+    
+
+    
+
+    alt= "Home" 
+src={ home }
+
+
+    
+
+
+/>
+<div
+    
+        className= "my-recepits" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<img
+    
+        className= "bookmark" 
+
+    
+
+    
+
+    alt= "Bookmark" 
+src={ bookmark3 }
+
+
+    
+
+
+/>
+<div
+    
+        className= "text-wrapper-3" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+Weigh in
+</div>
+</div>
+<div
+    
+        className= "offers" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<div
+    
+        className= "text-wrapper-4" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+leaderboard
+</div>
+<img
+    
+        className= "discount" 
+
+    
+
+    
+
+    alt= "Discount" 
+src={ discount1 }
+
+
+    
+
+
+/>
+</div>
+<div
+    
+        className= "message" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<img
+    
+        className= "chat" 
+
+    
+
+    
+
+    alt= "Chat" 
+src={ chat2 }
+
+
+    
+
+
+/>
+<div
+    
+        className= "text-wrapper-5" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+player stats
+</div>
+</div>
+<div
+    
+        className= "setting" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+<img
+    
+        className= "setting-2" 
+
+    
+
+    
+
+    alt= "Setting" 
+src={ setting }
+
+
+    
+
+
+/>
+<div
+    
+        className= "text-wrapper-6" 
+
+    
+
+    
+
+    
+
+    
+
+
+>
+Past Seasons
+</div>
+</div>
+<div
+    
+        className= "home-indicator" 
+
+    
+
+    
+
+    
+
+    
+
+
+/>
+</div>
+<img
+    
+        className= "frame-6" 
+
+    
+
+    
+
+    alt= "Frame" 
+src={ frame60 }
+
+
+    
+
+
+/>
+</div>
+</div>
+</div>
+)
+}
